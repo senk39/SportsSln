c730dff059470a338928e2f6e842e3499cf5173b
Senk <paweldawidziak@gmail.com>
2020-06-20 14:57:53 +0200

    Initialize Entity Framework and added sample data

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -14,9 +17,22 @@ namespace SportsStore
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        private IConfiguration Configuration { get; set; }
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts =>
+            {
+                opts.UseSqlServer(Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
