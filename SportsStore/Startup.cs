df50ae8abaf2cb232f83e889e47f429b9de2e155
Senk <paweldawidziak@gmail.com>
2020-06-30 12:57:40 +0200

    installed bootstrap

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -48,6 +48,7 @@ namespace SportsStore
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index" });
                 endpoints.MapDefaultControllerRoute();
             });
             SeedData.EnsurePopulated(app);
