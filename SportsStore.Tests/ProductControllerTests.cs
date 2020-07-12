0020c42964f5e95433c4ed6ea0d4eb0e114cfd6f
Senk <paweldawidziak@gmail.com>
2020-07-12 15:24:57 +0200

    Fixed routing

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Filter_Products()
+        {
+            //Arrage
+            // - create the mock repository
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category="Cat1" },
+                new Product {ProductID = 2, Name = "P2", Category="Cat2" },
+                new Product {ProductID = 3, Name = "P3", Category="Cat1" },
+                new Product {ProductID = 4, Name = "P4", Category="Cat2" },
+                new Product {ProductID = 5, Name = "P5", Category="Cat3" },
+            }).AsQueryable<Product>());
+
+            //Arrange 2 - create a controller and make the page size 3 items
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            Product[] result = (controller.Index("Cat2", 1).ViewData.Model as ProductsListViewModel).Products.ToArray();
+
+            //Assert
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+
+
+        }
+    }
+}
