62423ab8a6dfcc59154d591e550fd0604053da72
Senk <paweldawidziak@gmail.com>
2020-08-03 16:25:13 +0200

    I wrote unit tests for cart

---
+++ SportsStore.Tests/CartTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.DynamicProxy.Contributors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using SportsStore.Pages;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Can_Add_New_Lines()
+        {
+            //arrange - create some products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            //arrange - creatye a new cart
+            Cart target = new Cart();
+
+            //act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            Models.Cart.CartLine[] results = target.Lines.ToArray();
+
+            //assert
+
+            Assert.Equal(2, results.Length);
+            Assert.Equal(p1, results[0].Product);
+            Assert.Equal(p2, results[1].Product);
+
+        }
+
+        [Fact]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            //arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            //arrange cart
+            Cart target = new Cart();
+            //act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            Models.Cart.CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+
+            //assert
+
+            Assert.Equal(2, results.Length);
+            Assert.Equal(11, results[0].Quantity);
+            Assert.Equal(1, results[1].Quantity);
+        }
+
+        [Fact]
+        public void Can_Remove_Line()
+        {
+            //Arrange: Products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            //Arrange:  Cart
+            Cart target = new Cart();
+            //Arrange: add some products
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+            //Act
+            target.RemoveLine(p2);
+
+            //Assert
+            Assert.Empty(target.Lines.Where(c => c.Product == p2));
+            Assert.Equal(2, target.Lines.Count());
+        }
+
+        [Fact]
+        public void Calculate_Cart_Total()
+        {
+            //Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            //Arrange:  Cart
+            Cart target = new Cart();
+
+            //Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+            decimal result = target.ComputeTotalValue();
+
+            //Assert
+            Assert.Equal(450M, result);
+        }
+
+        [Fact]
+        public void Can_Clear_Contents()
+        {
+            //Arrange - create some products
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            //Arrange:  Cart
+            Cart target = new Cart();
+
+            //Arrange: add to cart
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            //Act: clear the cart
+            target.Clear();
+
+            //Assert
+            Assert.Empty(target.Lines);
+
+
+        }
+    }
+}
